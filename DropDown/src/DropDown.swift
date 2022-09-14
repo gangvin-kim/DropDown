030e984d95bdb7c5d8cc640a973037f9742eeca4
gangvin-kim <>
2022-09-14 14:34:10 +0900

    fix 4

--- DropDown/src/DropDown.swift
+++ DropDown/src/DropDown.swift
@@ -773,7 +773,7 @@ extension DropDown {
 		let anchorViewMaxY = anchorView?.plainView.windowFrame?.maxY ?? 0
 
 		let x = anchorViewX + topOffset.x
-        var y = (anchorViewMaxY + topOffset.y) - +(self.maxHeight ?? tableHeight)
+        var y = (anchorViewMaxY + topOffset.y) - tableHeight
 
 		let windowY = window.bounds.minY + DPDConstant.UI.HeightPadding
 
